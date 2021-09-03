cc7cdfddea1642f6795ec7e23b7882122701bd44
Anton Radzionau <radionoff.antony@gmail.com>
2021-09-03 22:27:24 +0300

    Security filter was added.

---
+++ src/main/java/by/radzionau/imdb/controller/filter/PageRedirectSecurityFilter.java
@@ -0,0 +1,29 @@
+package by.radzionau.imdb.controller.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = { "/pages/*" },
+        initParams = { @WebInitParam(name = "INDEX_PATH", value = "/index.jsp") })
+public class PageRedirectSecurityFilter implements Filter {
+    private String indexPath;
+    public void init(FilterConfig config) throws ServletException {
+        indexPath = config.getInitParameter("INDEX_PATH");
+        //indexPath = "/index.jsp";
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.sendRedirect(httpRequest.getContextPath() + indexPath);
+        chain.doFilter(request, response);
+    }
+}
