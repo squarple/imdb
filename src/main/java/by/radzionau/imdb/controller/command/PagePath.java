201fdcaedb978d7ae380bbc5c6a5e42112aef027
Anton Radzionau <radionoff.antony@gmail.com>
2021-09-08 11:19:29 +0300

    New page paths was added.

--- src/main/java/by/radzionau/imdb/controller/command/PagePath.java
+++ src/main/java/by/radzionau/imdb/controller/command/PagePath.java
@@ -3,7 +3,8 @@ package by.radzionau.imdb.controller.command;
 public enum PagePath {
     MAIN_PAGE ("pages/main.jsp"),
     LOGIN_PAGE ("pages/login.jsp"),
-    RESULT_PAGE ("pages/result.jsp"),
+    SIGNUP_PAGE("pages/signup.jsp"),
+    VERIFY_EMAIL_PAGE("pages/verify_email.jsp"),
     ERROR_500_PAGE ("pages/error/error500.jsp"),
     ERROR_404_PAGE ("pages/error/error404.jsp");
 
