569166889dfb714e62072ba7d4b9faf8127ad963
Anton Radzionau <radionoff.antony@gmail.com>
2021-09-11 08:21:54 +0300

    Initialization of pool was added.

--- src/main/java/by/radzionau/imdb/controller/listener/ServletContextListenerImpl.java
+++ src/main/java/by/radzionau/imdb/controller/listener/ServletContextListenerImpl.java
@@ -7,6 +7,13 @@ import jakarta.servlet.annotation.WebListener;
 
 @WebListener
 public class ServletContextListenerImpl implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        CustomConnectionPool.getInstance();
+        ServletContextListener.super.contextInitialized(sce);
+    }
+
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         CustomConnectionPool.getInstance().destroyPull();
