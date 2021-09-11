c83e3a7096db595b936e795d6074a344e9d8ee0a
Anton Radzionau <radionoff.antony@gmail.com>
2021-09-11 08:25:15 +0300

    New attributes was added.

--- src/main/java/by/radzionau/imdb/controller/command/RequestAttribute.java
+++ src/main/java/by/radzionau/imdb/controller/command/RequestAttribute.java
@@ -1,8 +1,13 @@
 package by.radzionau.imdb.controller.command;
 
-public class RequestAttribute {
+public final class RequestAttribute {
     public static final String USER = "user";
     public static final String ROLE = "role";
+    public static final String LOGIN = "login";
+    public static final String LOCALE = "locale";
+
+    public static final String PAGE_TO = "page_to";
+    public static final String PAGE_FROM = "page_from";
 
     private RequestAttribute() {
 
--- src/main/java/by/radzionau/imdb/controller/command/RequestParameter.java
+++ src/main/java/by/radzionau/imdb/controller/command/RequestParameter.java
@@ -4,11 +4,13 @@ public class RequestParameter {
     public static final String COMMAND = "command";
     public static final String LOGIN = "login";
     public static final String PASSWORD = "password";
+    public static final String REPEATED_PASSWORD = "repeated_password";
     public static final String EMAIL = "email";
     public static final String FIRST_NAME = "first_name";
     public static final String SURNAME = "surname";
 
     public static final String PAGE_TO = "page_to";
+    public static final String PAGE_FROM = "page_from";
 
     private RequestParameter() {
 
