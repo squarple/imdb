07abd0c8e45ae30db6643e29a095230cee68f03d
Anton Radzionau <radionoff.antony@gmail.com>
2021-09-27 16:56:51 +0300

    Singleton has been implemented.

--- src/main/java/by/radzionau/imdb/util/security/PasswordEncryptor.java
+++ src/main/java/by/radzionau/imdb/util/security/PasswordEncryptor.java
@@ -3,15 +3,20 @@ package by.radzionau.imdb.util.security;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class PasswordEncryptor {
+    private static final PasswordEncryptor INSTANCE = new PasswordEncryptor();
     private PasswordEncryptor() {
 
     }
 
-    public static String encryptPassword(String password) {
+    public static PasswordEncryptor getInstance() {
+        return INSTANCE;
+    }
+
+    public String encryptPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
 
-    public static boolean checkPassword(String password, String hashedPassword) {
+    public boolean checkPassword(String password, String hashedPassword) {
         return BCrypt.checkpw(password, hashedPassword);
     }
 }
