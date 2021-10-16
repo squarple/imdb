cf85c085738da63622af2d72219f0368e801304f
Anton Radzionau <radionoff.antony@gmail.com>
2021-10-16 23:04:40 +0300

    Tag have been added.

---
+++ src/main/java/by/radzionau/imdb/tag/CompanyNameTag.java
@@ -0,0 +1,29 @@
+package by.radzionau.imdb.tag;
+
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.JspWriter;
+import jakarta.servlet.jsp.tagext.TagSupport;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+/**
+ * The class CompanyNameTag.
+ */
+public class CompanyNameTag extends TagSupport {
+    private static final Logger logger = LogManager.getLogger();
+    private static final String COMPANY_NAME = "&copy 2021 IMDb, Inc";
+
+    @Override
+    public int doStartTag() throws JspException {
+        try {
+            JspWriter out = pageContext.getOut();
+            out.write(COMPANY_NAME);
+        } catch (IOException e) {
+            logger.error("CompanyNameTag error: {}", e.getMessage(), e);
+            throw new JspException("CompanyNameTag error: " + e.getMessage(), e);
+        }
+        return SKIP_BODY;
+    }
+}
